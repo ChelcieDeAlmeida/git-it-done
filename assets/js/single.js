766ae90dec67cc6a8536284c7ef1a684436105ca
XY <chelciedealmeida@gmail.com>
2021-03-23 02:34:58 -0700

    linked both html pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,24 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -16,15 +35,14 @@ var getRepoIssues = function (repo) {
                 }
 
             });
-        }
-        else {
-            alert("There was a problem with your request!");
+        } else {
+          // if not successful, redirect to homepage  
+            document.location.replace("./index.html");
         }
     });
 
 };
 
-getRepoIssues("facebook/react");
 
 var displayIssues = function (issues) {
 
@@ -79,3 +97,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
   };
 
+  getRepoName();
